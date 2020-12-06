7171d9a66fa2e616f71891b038565de0519b6840
patjagielski <jagielskipat@gmail.com>
2020-12-06 23:50:56 +0100

    exercise 5 somewhat done

--- Lab2B/refactoring-exercises/exercise-05/src/CsvWriter.java
+++ Lab2B/refactoring-exercises/exercise-05/src/CsvWriter.java
@@ -1,43 +1,62 @@
 public class CsvWriter {
+	private static char NEWLINE = '\n';
+	private static String COMMA = ",";
+	private static char SLASH = '\"';
+	private static String DOUBLESLASH = "\"\"";
+
+
+
+	private StringBuilder toReturn;
 	public CsvWriter() {
+		toReturn = new StringBuilder();
 	}
 
+	/**
+	 * Bad Smell: Message chain would be my guess
+	 * Originally where ever I had the toReturn call was a system.out.println call
+	 * each method returns something rather than manipulating some variable
+	 */
 	public void write(String[][] lines) {
 		for (int i = 0; i < lines.length; i++) {
 			writeLine(lines[i]);
 		}
+		System.out.println(toReturn);
 	}
 
-	private void writeLine(String[] fields) {
+	private StringBuilder writeLine(String[] fields) {
 		if (fields.length == 0)
-			System.out.println();
+			return toReturn.append(NEWLINE);
 		else {
-			writeField(fields[0]);
+			 writeField(fields[0]);
 
 			for (int i = 1; i < fields.length; i++) {
-				System.out.print(",");
+				toReturn.append(COMMA);
 				writeField(fields[i]);
 			}
-			System.out.println();
+			return toReturn.append(NEWLINE);
 		}
+
 	}
 
-	private void writeField(String field) {
-		if (field.indexOf(',') != -1 || field.indexOf('\"') != -1)
-			writeQuoted(field);
+	private StringBuilder writeField(String field) {
+		if (field.indexOf(COMMA) != -1 || field.indexOf(SLASH) != -1)
+			return writeQuoted(field);
 		else
-			System.out.print(field);
+			return toReturn.append(field);
+
 	}
 
-	private void writeQuoted(String field) {
-		System.out.print('\"');
+	private StringBuilder writeQuoted(String field) {
+		toReturn.append(SLASH);
 		for (int i = 0; i < field.length(); i++) {
 			char c = field.charAt(i);
-			if (c == '\"')
-				System.out.print("\"\"");
+			if (c == SLASH)
+				toReturn.append(DOUBLESLASH);
 			else
-				System.out.print(c);
+				toReturn.append(c);
 		}
-		System.out.print('\"');
+		return toReturn.append(SLASH);
 	}
+
+
 }
